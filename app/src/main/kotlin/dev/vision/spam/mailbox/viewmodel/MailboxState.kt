5328d21072e9a88892d8f341dfefb056ce26337a
Victor <victor@smartdigit.pt>
2024-03-29 01:12:37 +0200

    Added inboxes and loading fields

--- app/src/main/kotlin/dev/vision/spam/mailbox/viewmodel/MailboxState.kt
+++ app/src/main/kotlin/dev/vision/spam/mailbox/viewmodel/MailboxState.kt
@@ -2,10 +2,14 @@ package dev.vision.spam.mailbox.viewmodel
 
 import androidx.compose.runtime.Immutable
 import dev.vision.spam.mailbox.model.Email
+import dev.vision.spam.mailbox.model.Inbox
+import dev.vision.spam.mailbox.model.Message
 
 @Immutable
 data class MailboxState(
-    val emails: List<Email> = emptyList(),
-    val spam: Set<Email> = emptySet(),
-    val ham: Set<Email> = emptySet(),
+    val loading: Boolean = false,
+    val inboxes: List<Inbox> = emptyList(),
+    val messages: List<Message> = emptyList(),
+    val spam: Set<Message> = emptySet(),
+    val ham: Set<Message> = emptySet(),
 )
