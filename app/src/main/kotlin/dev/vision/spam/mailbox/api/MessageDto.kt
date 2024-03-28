0383db4ee2d47d0e095e10f031897405f991721e
Victor <victor@smartdigit.pt>
2024-03-29 01:13:44 +0200

    Added MessageDto

---
+++ app/src/main/kotlin/dev/vision/spam/mailbox/api/MessageDto.kt
@@ -0,0 +1,83 @@
+package dev.vision.spam.mailbox.api
+
+
+import dev.vision.spam.mailbox.model.Message
+import dev.vision.spam.mailbox.model.User
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+class MessageDto(
+    val id: Long,
+    @SerialName("inbox_id")
+    val inboxId: Int,
+    val subject: String,
+    @SerialName("sent_at")
+    val sentAt: String,
+    @SerialName("from_email")
+    val fromEmail: String,
+    @SerialName("from_name")
+    val fromName: String,
+    @SerialName("to_email")
+    val toEmail: String,
+    @SerialName("to_name")
+    val toName: String,
+    @SerialName("email_size")
+    val emailSize: Int,
+    @SerialName("is_read")
+    val isRead: Boolean,
+    @SerialName("created_at")
+    val createdAt: String,
+    @SerialName("updated_at")
+    val updatedAt: String,
+    @SerialName("html_body_size")
+    val htmlBodySize: Int,
+    @SerialName("text_body_size")
+    val textBodySize: Int,
+    @SerialName("human_size")
+    val humanSize: String,
+    @SerialName("html_path")
+    val htmlPath: String,
+    @SerialName("txt_path")
+    val txtPath: String,
+    @SerialName("raw_path")
+    val rawPath: String,
+    @SerialName("download_path")
+    val downloadPath: String,
+    @SerialName("html_source_path")
+    val htmlSourcePath: String,
+    @SerialName("blacklists_report_info")
+    val blacklistsReportInfo: BlacklistsReportInfo,
+    @SerialName("smtp_information")
+    val smtpInformation: SmtpInformation
+) {
+    @Serializable
+    class BlacklistsReportInfo(
+        val result: String,
+        val domain: String,
+        val ip: String,
+        val report: List<Report>
+    ) {
+        @Serializable
+        class Report(
+            val name: String,
+            val url: String,
+            @SerialName("in_black_list")
+            val inBlackList: Boolean
+        )
+    }
+
+    @Serializable
+    class SmtpInformation(
+        val ok: Boolean
+    )
+}
+
+fun MessageDto.toDomain(body: String) = Message(
+    from = User(
+        address = fromEmail,
+        name = fromName
+    ),
+    subject = subject,
+    body = body
+)
