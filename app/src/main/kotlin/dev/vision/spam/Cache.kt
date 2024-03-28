7d191237406797312b8310df7ea66aa13d8c7c45
Victor <victor@smartdigit.pt>
2024-03-29 01:11:02 +0200

    Created Cache

---
+++ app/src/main/kotlin/dev/vision/spam/Cache.kt
@@ -0,0 +1,20 @@
+package dev.vision.spam
+
+import android.content.Context
+import androidx.core.content.edit
+
+class Cache(context: Context) {
+    private val preferences by lazy {
+        context.getSharedPreferences("cache", Context.MODE_PRIVATE)
+    }
+
+    private object Keys {
+        const val MAILTRAP_KEY = "mailtrap_api_key"
+    }
+
+    fun saveKey(key: String) = preferences.edit {
+        putString(Keys.MAILTRAP_KEY, key)
+    }
+
+    fun getKey() = preferences.getString(Keys.MAILTRAP_KEY, null)
+}
