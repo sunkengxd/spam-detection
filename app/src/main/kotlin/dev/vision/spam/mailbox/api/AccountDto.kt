2487b5364c56f8bb50106ce5c09b5df2a8a31cdb
Victor <victor@smartdigit.pt>
2024-03-29 01:10:35 +0200

    Created AccountDto

---
+++ app/src/main/kotlin/dev/vision/spam/mailbox/api/AccountDto.kt
@@ -0,0 +1,13 @@
+package dev.vision.spam.mailbox.api
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+class AccountDto(
+    val id: Long,
+    val name: String,
+    @SerialName("access_levels")
+    val accessLevels: List<Int>
+)
