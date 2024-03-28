d612ba231a86fa4c96749fb83e2a19d28a92b339
Victor <victor@smartdigit.pt>
2024-03-29 01:12:23 +0200

    Added loading state handling

--- app/src/main/kotlin/dev/vision/spam/mailbox/ui/MailboxScreen.kt
+++ app/src/main/kotlin/dev/vision/spam/mailbox/ui/MailboxScreen.kt
@@ -1,10 +1,15 @@
 package dev.vision.spam.mailbox.ui
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.vision.spam.mailbox.viewmodel.MailboxViewModel
@@ -14,14 +19,20 @@ fun MailboxScreen(viewModel: MailboxViewModel, modifier: Modifier = Modifier) {
 
     val state by viewModel.state.collectAsStateWithLifecycle()
 
-    LaunchedEffect(Unit) {
-        viewModel.load()
+    AnimatedContent(targetState = state.loading, label = "") {
+        if (it) {
+            Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator(
+                    strokeCap = StrokeCap.Round
+                )
+            }
+        } else {
+            MessageList(
+                state = state,
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .then(modifier)
+            )
+        }
     }
-
-    EmailList(
-        state = state,
-        modifier = Modifier
-            .padding(horizontal = 20.dp)
-            .then(modifier)
-    )
 }
