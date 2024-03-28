568396a1f8b6049fd0982a74f44de0a2e1e1d696
Victor <victor@smartdigit.pt>
2024-03-29 01:13:09 +0200

    Created MailtrapApi

---
+++ app/src/main/kotlin/dev/vision/spam/mailbox/api/MailtrapApi.kt
@@ -0,0 +1,20 @@
+package dev.vision.spam.mailbox.api
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+
+class MailtrapApi(private val client: HttpClient) {
+
+    suspend fun accounts(): List<AccountDto> = client.get("/accounts").body()
+
+    suspend fun inboxes(accountId: Long): List<InboxDto> =
+        client.get("/accounts/$accountId/inboxes").body()
+
+    suspend fun messages(accountId: Long, inboxId: Long): List<MessageDto> =
+        client.get("/accounts/${accountId}/inboxes/${inboxId}/messages").body()
+
+    suspend fun body(accountId: Long, inboxId: Long, messageId: Long): String =
+        client.get("/accounts/${accountId}/inboxes/${inboxId}/messages/$messageId/body.txt").bodyAsText()
+}
