d52c72afcb6b30f8aa1796d56315cc3c219427d4
Victor <victor@smartdigit.pt>
2024-03-29 01:13:19 +0200

    Added auth flow

--- app/src/main/kotlin/dev/vision/spam/MainActivity.kt
+++ app/src/main/kotlin/dev/vision/spam/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -15,7 +19,9 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import dev.vision.spam.classifier.randomSpamClassifier
-import dev.vision.spam.mailbox.repository.emailRepository
+import dev.vision.spam.mailbox.api.MailtrapApi
+import dev.vision.spam.mailbox.api.client
+import dev.vision.spam.mailbox.repository.MailtrapEmailRepository
 import dev.vision.spam.mailbox.ui.MailboxScreen
 import dev.vision.spam.mailbox.viewmodel.MailboxViewModel
 import dev.vision.spam.ui.component.MainTopAppBar
@@ -23,24 +29,57 @@ import dev.vision.spam.ui.theme.SpamTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val cache by lazy {
+        Cache(this)
+    }
+
+    private val client by lazy {
+        client(cache)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val viewModel: MailboxViewModel = viewModel(
-                factory = viewModelFactory {
-                    initializer {
-                        MailboxViewModel(emailRepository(), randomSpamClassifier)
-                    }
-                }
-            )
+            var apiKey: String? by remember {
+                mutableStateOf(null)
+            }
+
+            LaunchedEffect(Unit) {
+                apiKey = cache.getKey()
+            }
+
+            val loggedIn = remember(apiKey) {
+                apiKey != null
+            }
             SpamTheme {
-                Scaffold(
-                    topBar = {
-                        MainTopAppBar(onClickSort = viewModel::sort)
+                AnimatedContent(targetState = loggedIn, label = "") {
+                    if (it) {
+                        val viewModel: MailboxViewModel = viewModel(
+                            factory = viewModelFactory {
+                                initializer {
+                                    MailboxViewModel(
+                                        repository = MailtrapEmailRepository(MailtrapApi(client)),
+                                        classifier = randomSpamClassifier
+                                    )
+                                }
+                            }
+                        )
+                        Scaffold(
+                            topBar = {
+                                MainTopAppBar(
+                                    onClickSort = viewModel::sort,
+                                    onClickRefresh = viewModel::load
+                                )
+                            }
+                        ) { scaffoldPadding ->
+                            MailboxScreen(viewModel, Modifier.padding(scaffoldPadding))
+                        }
+                    } else {
+                        Surface(Modifier.fillMaxSize()) {
+                            AuthScreen({ apiKey = cache.getKey() }, cache)
+                        }
                     }
-                ) { scaffoldPadding ->
-                    MailboxScreen(viewModel, Modifier.padding(scaffoldPadding))
                 }
             }
         }
